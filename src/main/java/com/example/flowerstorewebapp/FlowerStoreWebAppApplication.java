cc1644bb79a5987ef1eadda1bf49b4a5277da281
Maximilian Coreachin <maxcorechin@Maximilians-MacBook-Air.local>
2024-03-09 17:17:25 -0500

    Add project structure

---
+++ src/main/java/com/example/flowerstorewebapp/FlowerStoreWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowerstorewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerStoreWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerStoreWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flowerstorewebapp/FlowerStoreWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowerstorewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerStoreWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
